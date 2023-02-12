65490b6ec5810618be0024bae46f51dc909dff06
Hugh Baxter <hughdbaxter@gmail.com>
2023-02-12 13:41:26 +0000

    Error when deserializing a Key now raises a de::Error instead of panicking

--- src/config/deserialize.rs
+++ src/config/deserialize.rs
@@ -2,11 +2,10 @@
 // i.e. Map and Key, so that they can loaded from config.
 
 use std::fmt::Debug;
+use std::str::FromStr;
 
 use crate::device::Key;
 use crate::errors::ConfigError;
-use crate::mapping::Map;
-use std::str::FromStr;
 
 impl Debug for Key {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -31,14 +30,17 @@ impl<'de> serde::de::Visitor<'de> for KeyVisitor {
     type Value = Key;
 
     fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-        formatter.write_str("a valid key descriptor string i.e. 'KEY_A'")
+        formatter.write_str("a string corresponding to a key i.e. 'KEY_A'")
     }
 
     fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
     where
         E: serde::de::Error,
     {
-        Ok(Key::from_str(v).unwrap())
+        match Key::from_str(v) {
+            Ok(k) => Ok(k),
+            Err(e) => Err((serde::de::Error::custom(e))),
+        }
     }
 }
 
